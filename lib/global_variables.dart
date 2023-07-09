e553b1f84ec051610fdf197a077e6034bebc59ea
ANKIT <ankitpaul894@gmail.com>
2023-07-09 12:07:08 +0530

    global variables with prefferences added

---
+++ lib/global_variables.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+List<String> preferences = [
+  "Buy a Car",
+  "Buy a Bike",
+  "Fashion & Style",
+  "Books and Magazines",
+  "Health and Wellness",
+  "Tech & Gadgets",
+  "Travel and Vacation",
+  "Add custom preference",
+];
+
+List<IconData> icons = [
+  FontAwesomeIcons.car,
+  FontAwesomeIcons.motorcycle,
+  FontAwesomeIcons.shirt,
+  FontAwesomeIcons.book,
+  FontAwesomeIcons.heartPulse,
+  FontAwesomeIcons.mobile,
+  FontAwesomeIcons.planeDeparture,
+  FontAwesomeIcons.bagShopping,
+];
--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
