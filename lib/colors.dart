8d49eb3bf1944503e2c4fc3596c5541917430942
arnab7070 <arnabnandi7074@gmail.com>
2023-07-09 12:21:54 +0530

    Colors.dart added

---
+++ lib/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+MaterialColor myPrimarySwatch = const MaterialColor(
+  0xFF651FE1,
+  <int, Color>{
+    50: Color(0xFFE8E4F8),
+    100: Color(0xFFCDC7F0),
+    200: Color(0xFFB0A9E7),
+    300: Color(0xFF927BE0),
+    400: Color(0xFF7A58DC),
+    500: Color(0xFF651FE1),
+    600: Color(0xFF5C1AD9),
+    700: Color(0xFF5014CD),
+    800: Color(0xFF460FC4),
+    900: Color(0xFF3308B6),
+  },
+);
