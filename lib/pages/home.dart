6afb33cd199125934a07d91c7194ab19f5f64047
bishal <bishalkarmakar007@gmail.com>
2023-07-09 12:18:54 +0530

    scroll crontroller + hidable

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:dream_tracker/widgets/home_body.dart';
 import 'package:flutter/material.dart';
+import 'package:hidable/hidable.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,19 +10,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ScrollController _controller = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //
+      //____APP BAR And Body
+      //
       body: NestedScrollView(
+        controller: _controller,
         floatHeaderSlivers: true,
         headerSliverBuilder: (context, innerBoxIsScrolled) {
           return [
+            //
+            //____APP BAR
+            //
             const SliverAppBar(
               title: Text('Dream Tracker'),
             ),
           ];
         },
-        body: HomeBody(),
+        //
+        //____ Body
+        //
+        body: const HomeBody(),
+      ),
+      //
+      //____Button
+      //
+      floatingActionButton: Hidable(
+        controller: _controller,
+        child: FloatingActionButton(onPressed: () {}),
       ),
     );
   }
