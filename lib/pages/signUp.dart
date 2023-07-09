db4ccf11b66ce9cd4831c632028c30157eea6ec8
neo_deus <arpandesp7890@gmail.com>
2023-07-09 13:38:38 +0530

    login

--- lib/pages/signUp.dart
+++ lib/pages/signUp.dart
@@ -35,7 +35,33 @@ class LoginScreen extends StatelessWidget {
 
   Future<String?> _signupUser(SignupData data) {
     debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
+    return Future.delayed(loginTime).then((_) async {
+      try {
+        UserCredential userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: data.name!,
+          password: data.password!,
+        );
+
+        if (userCredential.user != null) {
+          //created the user database
+          //await addUser();
+          // User is created, but email is not yet verified
+          // Send a verification email to the user
+          await userCredential.user!.sendEmailVerification();
+
+          // Return a message to inform the user that they need to verify their email
+          return 'Verification email has been sent. Please check your inbox.';
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          return 'The password provided is too weak.';
+        } else if (e.code == 'email-already-in-use') {
+          return 'The account already exists for that email.';
+        }
+      } catch (e) {
+        print(e);
+      }
       return null;
     });
   }
